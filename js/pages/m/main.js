d7dd58a80e5123f3625bc90dccee2ef9cbe3e71c
RDIL <34555510+RDIL@users.noreply.github.com>
2018-11-02 08:24:39 -0400

    Update main.js

--- js/pages/m/main.js
+++ js/pages/m/main.js
@@ -1,1 +1,29 @@
+$(function() {
+  const CONTRIBUTORS_URL = 'https://api.github.com/repos/HyperiumClient/Hyperium/contributors';
 
+  fetchJson(CONTRIBUTORS_URL, function(data) {
+    const contributors = data.filter(function(contributor) {
+      return contributor.contributions >= 20;
+    }).map(function(contributor) {
+      return {name: contributor.login, profileUrl: contributor.html_url, contributions: contributor.contributions};
+    });
+
+    const containerElement = document.getElementById('contributors-container');
+
+    contributors.forEach(function(contributor) {
+      const element = document.createElement('div');
+      element.className = 'card';
+      element.innerHTML = '<div class="card-body">'
+        + '<h5 class="card-title">' + contributor.name + '</h5>'
+        + '<p class="card-text">Contributions: ' + contributor.contributions + '</p>'
+        + '<a class="card-link" href="' + contributor.profileUrl + '">GitHub</a>'
+        + '</div>';
+      containerElement.appendChild(element);
+    });
+  });
+
+  $('body').scrollspy({
+    target: 'nav',
+    offset:50
+  });
+});
