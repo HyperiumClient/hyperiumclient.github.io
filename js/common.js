b86f720db91aa5d147ef0bd9a99c076d0f14ce87
asbyth <cyronize@gmail.com>
2019-10-09 15:05:35 -0400

    update website to be simpler & include more mods

--- js/common.js
+++ js/common.js
@@ -1,35 +1,35 @@
 function fetchJson(url, callback) {
-  var httpRequest = window.XMLHttpRequest
-    ? new XMLHttpRequest()
-    : new ActiveXObject("Microsoft.XMLHTTP");
-  httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        callback(JSON.parse(httpRequest.responseText));
-      } else {
-        callback(undefined, new Error(httpRequest.statusText));
-      }
-    }
-  };
-  httpRequest.open("GET", url);
-  httpRequest.send();
+    var httpRequest = window.XMLHttpRequest
+        ? new XMLHttpRequest()
+        : new ActiveXObject("Microsoft.XMLHTTP");
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                callback(JSON.parse(httpRequest.responseText));
+            } else {
+                callback(undefined, new Error(httpRequest.statusText));
+            }
+        }
+    };
+    httpRequest.open("GET", url);
+    httpRequest.send();
 }
 
-(function() {
-  document
-    .querySelectorAll(".mdc-button, .mdc-list-item")
-    .forEach(el => new mdc.ripple.MDCRipple(el));
-  const topAppBar = new mdc.topAppBar.MDCTopAppBar(
-    document.querySelector(".mdc-top-app-bar")
-  );
-  const drawer = mdc.drawer.MDCDrawer.attachTo(
-    document.querySelector(".mdc-drawer")
-  );
+(function () {
+    document
+        .querySelectorAll(".mdc-button, .mdc-list-item")
+        .forEach(el => new mdc.ripple.MDCRipple(el));
+    const topAppBar = new mdc.topAppBar.MDCTopAppBar(
+        document.querySelector(".mdc-top-app-bar")
+    );
+    const drawer = mdc.drawer.MDCDrawer.attachTo(
+        document.querySelector(".mdc-drawer")
+    );
 
-  drawer.foundation_.handleScrimClick = () => {
-    drawer.open = !drawer.open;
-  };
-  topAppBar.listen("MDCTopAppBar:nav", () => {
-    drawer.open = !drawer.open;
-  });
+    drawer.foundation_.handleScrimClick = () => {
+        drawer.open = !drawer.open;
+    };
+    topAppBar.listen("MDCTopAppBar:nav", () => {
+        drawer.open = !drawer.open;
+    });
 })();
