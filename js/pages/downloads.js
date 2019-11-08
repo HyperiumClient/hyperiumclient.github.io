dc27ced8725939b6a8f2462a88c10172c50cf52e
asbyth <cyronize@gmail.com>
2019-11-08 02:57:07 -0500

    update website for 1.2

--- js/pages/downloads.js
+++ js/pages/downloads.js
@@ -6,7 +6,7 @@
             .querySelector("div#latest-jar-version-card a.mdc-button")
             .setAttribute(
                 "href",
-                "https://sk1er.club/file_download?url=https://static.sk1er.club/hyperium_files/Hyperium%20Installer%203.0%20-%201.1.jar"
+                "https://sk1er.club/file_download?url=https://static.sk1er.club/hyperium_files/Hyperium%20Universal%20Installer.jar"
             );
         let versionString = "Version " + latestVersion.build;
         versionString += " build " + latestVersion.id + "<br/><b>Requires Java 8!</b>";
@@ -32,7 +32,7 @@
             .querySelector("div#latest-exe-version-card a.mdc-button")
             .setAttribute(
                 "href",
-                "https://sk1er.club/file_download?url=https://static.sk1er.club/hyperium_files/Hyperium%20Installer%20executable%203.0%20-%201.1.exe"
+                "https://sk1er.club/file_download?url=https://static.sk1er.club/hyperium_files/Hyperium%20Executable%20Installer.exe"
             );
         let versionString = "Version " + latestVersion.build;
         versionString += " build " + latestVersion.id;
